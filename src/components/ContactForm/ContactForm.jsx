dd96e9c9889e78697eb3c865d15798c76221e3af
s1dexdev <s1dexdev@gmail.com>
2021-03-28 22:18:01 +0300

    fixed addContact

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,7 +4,7 @@ import actions from '../../redux/actions';
 import PropTypes from 'prop-types';
 import styles from './ContactForm.module.css';
 
-const ContactForm = function ({ onSubmit }) {
+const ContactForm = function ({ items, onSubmit }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
@@ -29,8 +29,15 @@ const ContactForm = function ({ onSubmit }) {
   const handleSubmit = event => {
     event.preventDefault();
 
-    onSubmit(name, number);
-    resetState();
+    const isAlreadyInContacts = items.some(item => item.name === name);
+
+    if (isAlreadyInContacts) {
+      resetState();
+      return alert(`${name} is already in contacts`);
+    } else {
+      resetState();
+      return onSubmit(name, number);
+    }
   };
 
   return (
@@ -72,10 +79,14 @@ ContactForm.propTypes = {
   number: PropTypes.string,
 };
 
+const mapStateToProps = state => ({
+  items: state.contacts.items,
+});
+
 const mapDispatchToProps = dispatch => {
   return {
     onSubmit: (name, number) => dispatch(actions.addContact(name, number)),
   };
 };
 
-export default connect(null, mapDispatchToProps)(ContactForm);
+export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,16 +3,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import actions from './actions';
 
 const items = createReducer([], {
-  [actions.addContact]: (state, { payload }) => {
-    const isAlreadyInContacts = state.some(({ name }) => name === payload.name);
-
-    if (isAlreadyInContacts) {
-      return alert(`${payload.name} is already in contacts`);
-    } else {
-      return [...state, payload];
-    }
-  },
-
+  [actions.addContact]: (state, { payload }) => [...state, payload],
   [actions.deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
